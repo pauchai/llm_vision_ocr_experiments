4e952c30338244859a0f28bbe22e3ec3098b0ebc
Pavel Khimyack <paul.khimyack@gmail.com>
2025-05-06 15:13:30 +0500

    qwen gradio

--- qwen/main.py
+++ qwen/main.py
@@ -5,11 +5,17 @@ import time
 from io import BytesIO
 from PIL import Image
 import os
-from corrector import PhraseCorrectorNgrams, correct_text
+from unsloth import FastVisionModel # FastLanguageModel for LLMs
+import torch
+from pathlib import Path
 
-#OLLAMA_HOST = "http://ollama:11434"
-OLLAMA_CHAT_URL = os.environ.get("OLLAMA_BASE_URL")# f"{OLLAMA_HOST}/api/chat"
-OLLAMA_TAGS_URL = os.environ.get("OLLAMA_MODEL_URL")#f"{OLLAMA_HOST}/api/tags"
+base_data_dir = Path("/data")
+
+model_mapper = {
+  
+      "Qwen2_5-3b_finetuned":base_data_dir / "models/qwen2_5_3b_outputs",
+      "Qwen2_5-3b":"unsloth/Qwen2.5-VL-3B-Instruct"  
+}
 
 SYSTEM_PROMPT = '''
 Вы — ассистент OCR , который извлекает информацию с русских товарных этикеток на изображениях. В тексте могут встречаться названия на английском.
@@ -19,79 +25,77 @@ USER_PROMPT = '''
 Пожалуйста, извлеките весь текст на изображении и ничего больше без комментариев. 
 '''
 
-corrector = PhraseCorrectorNgrams(
-    score_cutoff=80,
-    min_len=3,
-    max_ngram=3
-)
 def get_available_models():
-    try:
-        response = requests.get(OLLAMA_TAGS_URL)
-        response.raise_for_status()
-        tags = response.json().get("models", [])
-        return [model["name"] for model in tags]
-    except Exception as e:
-        print(f"Ошибка при получении моделей: {e}")
-        return ["gemma3:1b"]  # дефолтная модель на случай ошибки
+    return model_mapper.keys()
+
+def load_model(model_name):
+    
+    return = FastVisionModel.from_pretrained(
+        model_mapper[model_name], # Model name
+        load_in_4bit = True, # Use 4bit to reduce memory use. False for 16bit LoRA.
+        use_gradient_checkpointing = "unsloth", # True or "unsloth" for long context
+    )
+
+def process_ocr(image_path, model, temperature = 1.5):
+    return recognized_text, time_end - time_start
 
 
-def encode_image(image: Image.Image) -> str:
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-
-def chat_with_ollama(system_prompt, user_prompt, image, selected_model, temperator=0.2, correct_text_flag=False):
+def chat_with_qwen(system_prompt, user_prompt, image:Image, selected_model, temperature=0.2):
+    model ,tokenizer = load_model(selected_model)
+    FastVisionModel.for_inference(model)  # Enable for inference!
     messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt}
+        {"role": "system",
+          "content": system_prompt
+        },
+        {"role": "user", "content": [
+            {"type": "image"},
+            {"type": "text", "text": user_prompt}
+        ]}
     ]
+    input_text = tokenizer.apply_chat_template(messages, add_generation_prompt=True)
 
-    if image:
-        image_base64 = encode_image(image)
-        messages[1]["images"] = [image_base64]
+    inputs = tokenizer(
+        [image],  # Убедитесь, что это список
+        [input_text],
+        add_special_tokens=False,
+        return_tensors="pt",
+    ).to("cuda")
 
-    ollama_args = {
-        "model": selected_model,
-        "messages": messages,
-        "stream": False,
-        "temperature": temperator,
-        "max_tokens": 200
-    }
+    input_ids = inputs["input_ids"][0]
 
-    start_time = time.time()  # Start timing
-    response = requests.post(OLLAMA_CHAT_URL, json=ollama_args)
-    elapsed_time = time.time() - start_time  # Calculate elapsed time
+    output_ids = model.generate(
+        **inputs,
+        max_new_tokens=128,
+        use_cache=True,
+        temperature=temperature,
+        min_p=0.1,
+    )[0]
 
-    if response.status_code == 200:
-        
-        result = response.json()["message"]["content"]
-        if correct_text_flag:
+    # Удаляем prompt, оставляя только сгенерированное
+    generated_only_ids = output_ids[len(input_ids):]
 
-            # Создаем экземпляр класса PhraseCorrectorNgrams
 
-            # Корректируем текст
-            result, corrections_log = correct_text(result, corrector)
-            print("Корректированный текст:", result)  # Added print statement for corrected text
-            print("Лог исправлений:", corrections_log)  # Added print statement for corrections log
-            return f"{result} (corrected)"
-        return f"{result}"
-    else:
-        return f"Ошибка: {response.status_code}\n{response.text}\n\nВремя запроса: {elapsed_time:.2f} секунд"
+    # Преобразуем выход модели в текст
+    recognized_text = tokenizer.decode(generated_only_ids, skip_special_tokens=True)
 
+    
+   
+
+    return f"{recognized_text}"
+    
 
 # Получаем список моделей из Ollama при запуске
 available_models = get_available_models()
 
 iface = gr.Interface(
-    fn=chat_with_ollama,
+    fn=chat_with_qwen,
     inputs=[
         gr.Textbox(label="Системный промпт", value=SYSTEM_PROMPT, placeholder="Введите системный промпт (необязательно)"),
         gr.Textbox(label="Ваш запрос", value=USER_PROMPT, placeholder="Введите ваш запрос"),
         gr.Image(type="pil", label="Изображение (необязательно)"),
         gr.Dropdown(choices=available_models,  label="Выберите модель"),
         gr.Slider(minimum=0, maximum=1, step=0.05, value=0.2, label="Temperature"),
-        gr.Checkbox(label="Корректировать текст?", value=False)
 
         
     ],
